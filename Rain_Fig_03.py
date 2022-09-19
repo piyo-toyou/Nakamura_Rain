59bd5e2faa8dc3265d10ed99a40fca6c3e806193
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2022-09-19 13:08:04 +0900

    stage2.2

--- Rain_Fig_03.py
+++ Rain_Fig_03.py
@@ -19,20 +19,25 @@ DataPATH = "G:/マイドライブ/Nakamura_Rain/"
 # 図のフォントの設定
 mpl.rcParams['font.family'] = 'MS Gothic'
 
-# 図の作成
-df1   = pd.read_excel(DataPATH + "Nakamura_Rain.xlsx", header=0, index_col=0)
-x     = np.array(df1.columns.values)
-y1   = np.array(df1.loc['窪川・時間'])
-y2   = np.array(df1.loc['窪川・連続'])
+# データの抽出
+df1  = pd.read_excel(DataPATH + "Nakamura_Rain_fig1.xlsx", header=None, index_col=0)
+x    = np.array(df1.index.values[2:])
+y1   = np.array(df1.iloc[2:, 2]) #列数を変更する
+y2   = np.array(df1.iloc[2:, 3]) #列数を変更する
 
 # 作図範囲の設定
-n = 36
+n = 360
 if len(x) > n:
     x = np.delete(x, slice(0, -n))
     y1 = np.delete(y1, slice(0, -n))
     y2 = np.delete(y2, slice(0, -n))
 else:
     pass
+
+# データの時系列反転
+x    = np.flipud(x)
+y1   = np.flipud(y1)
+y2   = np.flipud(y2)
 
 # 図・座標軸の設定
 fig = plt.figure(figsize=(10,6))
@@ -44,7 +49,7 @@ fig.subplots_adjust(bottom=0.35)
 ax1.bar(x, y1,
  width=0.2, color="navy", label="時間雨量（mm）")
 ax2.plot(x, y2,
- linestyle = "solid", linewidth = 1.0, c="tomato", marker='o', label="連続雨量（mm）")
+ linestyle = "solid", linewidth = 1.0, c="red", marker='.', label="連続雨量（mm）")
 
 # x軸,y軸の範囲
 ax1.set_ylim(0, max(y1)+10)
@@ -55,12 +60,13 @@ ax1.set_ylabel('時間雨量（mm）', fontsize=10, labelpad=5)
 ax2.set_ylabel('連続雨量（mm）', fontsize=10, labelpad=5)
 
 # 軸メモリラベル
-ax1.set_xticks(x)
-ax1.set_xticklabels(x, rotation=90)
+split_num = 10 #抽出間隔を指定
+ax1.set_xticks(x[::split_num])
+ax1.set_xticklabels(x[::split_num], rotation=90)
 
 # 凡例の有無・大きさ・位置
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
-ax1.legend(h1 + h2, l1 + l2, fontsize=10, edgecolor="black")
+ax1.legend(h1 + h2, l1 + l2, fontsize=10, loc="upper left", edgecolor="black")
 
 plt.show() 
